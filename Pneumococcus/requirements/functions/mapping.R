0c048c29c82779e6031470d64719cf4e17ce1c71
BaronWolfgang <jan.paul.van.meenen@gmail.com>
2025-03-07 15:07:39 +0100

    reworked the mapping function to instead find the exact longest continuous substring, locate the positions in the substrings and calculate the shift and required adjustment from that. Created 01_variability to process the variability data in aln.fas format. Updated 02_visualization to join the variability frequencies and join them to the prediction data

---
+++ Pneumococcus/requirements/functions/mapping.R
@@ -0,0 +1,84 @@
+###
+# Mapping functions to map positions in a given sequence, back to residue numbers in a pdb object.
+###
+
+## function map.seqposno.to.resno
+map.seqposno.to.resno <- function(sequence_AA1, pdb_object, verbose=FALSE) {
+  subject <- pdbseq(pdb_object)
+  unique_subject <- subject[!duplicated(names(subject))]
+  subject_resno <- as.numeric(names(unique_subject))
+  subject_seq <- str_c(
+    unname(unique_subject),
+    collapse = ""
+  )
+  
+  pattern_seq <- sequence_AA1
+  
+  #custom substitution matrix to find the longest continuous substring (LCS) without gaps or mismatches
+  customsubmatrix <- matrix(-Inf, 
+                            nrow = 20, 
+                            ncol = 20, 
+                            dimnames = list(AA_ALPHABET[1:20],AA_ALPHABET[1:20]))
+  diag(customsubmatrix) <- 1
+  
+  #align to find the LCS
+  alignment <- pairwiseAlignment(pattern_seq, subject_seq, type="local", 
+                                 substitutionMatrix = customsubmatrix)
+  
+  #extract the LCS
+  aligned_pattern <- as.character(alignment)
+  
+  #locate the LCS in both strings
+  loc_subject <- str_locate(subject_seq,pattern = aligned_pattern)
+  loc_pattern <- str_locate(pattern_seq,pattern = aligned_pattern)
+  
+  #Find the difference in the position of the LCS
+  shift <- loc_subject[1] - loc_pattern[1] 
+  
+  #Look at the first resno number in the pdb file, add the calculated shift
+  required_adjustment <- subject_resno[1] - 1 + shift
+  
+  #verbose
+  if(verbose) {
+    print(aligned_pattern)
+    print(loc_subject)
+    print(alignedSubject(alignment)) 
+    print(loc_pattern)
+    print(alignedPattern(alignment)) 
+    print(shift)
+  }
+  
+  return(c(required_adjustment))
+}
+
+## map.seqresno.to.resno
+library("Biostrings")
+
+map.seqresno.to.resno <- function(pdb_object, verbose=FALSE) {
+  subject_seq <- pdb_object$atom %>%
+    filter(resid %in% toupper(AMINO_ACID_CODE)) %>%
+    select(resno, resid) %>%
+    distinct(.keep_all = TRUE) %>%
+    mutate(resid = aa321(resid)) %>%
+    pull(resid) %>%
+    str_c(collapse = "")
+  
+  required_adjustment <- map.seqposno.to.resno(subject_seq, pdb_object, verbose=verbose)
+  
+  return(c(required_adjustment))
+}
+
+# create resmap
+create.resmap <- function(pdb_atom_df, epitope_prediction_df,required_adjustment, join_by = c("resno")) {
+  resmap_df <- pdb_atom_df %>% 
+    select(resid,resno,chain) %>%
+    unique() %>%
+    filter(resid %in% toupper(AMINO_ACID_CODE),
+           !is.na(chain),
+           chain != FALSE) %>%
+    full_join(epitope_prediction_df %>%
+                mutate(resno = Position + required_adjustment),
+              by = join_by
+              )
+  return(resmap_df)
+}
---
+++ Pneumococcus/requirements/packages.R
@@ -0,0 +1,27 @@
+data_management_packages_to_install <- c("data.table", "tidyverse" ,"readxl","purrr","readr","scales","msa","jsonlite")
+data_visualisation_packages_to_install <- c("ggplot2","cowplot","pheatmap","viridisLite","ggseqlogo")
+structural_packages_to_install <- c("bio3d","NGLVieweR", "htmlwidgets","htmltools")
+
+packages_to_install <- c(data_management_packages_to_install, 
+                         data_visualisation_packages_to_install,
+                         structural_packages_to_install)
+
+# Check and install packages using a for loop
+for (package in packages_to_install) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+  library(package, character.only = TRUE)
+}
+
+BiocManager_packages_to_install <- c("Biostrings","pwalign")
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+for (package in BiocManager_packages_to_install) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    BiocManager::install(package)
+  }
+  library(package, character.only = TRUE)
+}
